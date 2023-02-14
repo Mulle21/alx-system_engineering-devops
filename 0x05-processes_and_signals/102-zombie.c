1ffb493cd3b83bfbe2bf57e779bb5cab7888016e
Mulle21 <110990945+Mulle21@users.noreply.github.com>
2023-02-14 17:34:51 +0300

    Update 102-zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 
 /**
- * infinite_while - creates an infinite loop to make the program hang *
+ * infinite_while - creates an infinite loop to make the program hang
  * Return: always 0
  */
 int infinite_while(void)
@@ -31,7 +31,7 @@ int main(void)
 			return (0);
 		printf("Zombie process created, PID: %d\n", zombie);
 	}
-	
-	infinite_while()
+
+	infinite_while();
 	return (0);
 }
