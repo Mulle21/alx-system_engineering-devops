9c5af795b16b596a9e3f639c6d78c9754b9a9509
Mulle21 <mulukenab95@gmail.com>
2023-09-16 13:47:13 -0700

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -12,3 +12,8 @@ content =>"
 	PasswordAuthentication no
 	",
 }
+# Set up the ssh configuration file
+exec { 'configuration_file':
+  command => 'sed -i "s|PasswordAuthentication yes|PasswordAuthentication no", /etc/ssh/ssh_config',
+  path    => '/bin',
+}
