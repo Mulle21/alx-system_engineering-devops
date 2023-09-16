0d0e74605f94584cf02a73945b01e66cd0ca8c88
Mulle21 <mulukenab95@gmail.com>
2023-09-16 13:30:51 -0700

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,10 @@
-##!/usr/bin/env bash
-# using puppet to make changes to configuration
+# ssh setup
 
-file { 'etc/ssh/ssh_cofig':
-	ensure => present,
-
-content =>"
-
-	#ssh clinet configuration
-	host*
-	IdentityFile ~/.ssh/school
-	passwordAuthentication no
-",
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/holberton',
 }
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
