9d16aff265755bcc50d2dbb02c887656e3c6f4a8
Mulle21 <mulukenab95@gmail.com>
2023-12-11 11:00:24 -0800

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# creates a file in /tmp
+# This Puppet manifest creates a file in /tmp
 
-file { '/tmp/school':
-  content =>'I love Puppet',
+file { 'holberton':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
+  content => 'I love Puppet'
 }
