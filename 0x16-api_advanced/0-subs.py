e317cd96a6ca2c17b53fd87e15812eecc79deaba
Mulle21 <mulukenab95@gmail.com>
2024-01-16 09:51:15 -0800

    sub many

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/reddit_api/project)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
